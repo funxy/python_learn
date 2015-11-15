121ab5689937c65c46c5a088d99e60397a6726d9
中村明 <rectron555@gmail.com>
2015-11-15 23:55:50 +0900

    tornado の勉強
    HTML と CSS を使って文字出力するように変更

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 """ tornado の勉強	
 """
-
+import os
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("hello world")
-
-application = tornado.web.Application([
-	(r"/", MainHandler),
-])
+		self.render("index.html")
 
 if __name__ == '__main__':
+	app = tornado.web.Application([
+		(r"/", MainHandler),
+	],
+	template_path = os.path.join(os.getcwd(), "templates"),
+	static_path = os.path.join(os.getcwd(), "static"),
+	)
+	app.listen(8888)
 	print("app run!!")
-	application.listen(8080)
 	tornado.ioloop.IOLoop.instance().start()
